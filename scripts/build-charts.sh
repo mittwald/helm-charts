b8924ca64fba20ab99d331de8821e29d6fbd7360
Dennis Hermsmeier <d.hermsmeier@mittwald.de>
2020-03-17 10:20:54 +0100

    refactor release process (#1)
    * manual triggerable workflow

--- scripts/build-charts.sh
+++ scripts/build-charts.sh
@@ -11,9 +11,9 @@ if [[ -z "${CHART_DESTINATION}" ]]; then
 fi
 mkdir -p "${CHART_DESTINATION}"
 
-CHART_REPOS=(${@:2})
-if [[ ${#CHART_REPOS[@]} -eq 0 ]]; then
-    echo "\${CHART_REPOS} is empty"
+CHART_REPOS="${2}"
+if [[ ! -f "${CHART_REPOS}" ]]; then
+    echo "\${CHART_REPOS} is not a file"
     exit 1
 fi
 
@@ -29,17 +29,25 @@ function cleanUp() {
 
 trap cleanUp 0 1 2 3 6 15
 
-for cR in ${CHART_REPOS[@]}; do
-    GITHUB_REPOSITORY="$(echo "${cR}" | cut -d ':' -f 1)"
+for cR in $(jq -r '.[] | @base64' "${CHART_REPOS}"); do
+    _jq() {
+     echo "${cR}" | base64 --decode | jq -r "${1}"
+    }
+
+    GITHUB_REPOSITORY="$(_jq '.repository')"
+    if [[ -z "${GITHUB_REPOSITORY}" ]]; then
+        echo "unable to determine '.repository'-key in ${CHART_REPOS}"
+        continue
+    fi
     GITHUB_URL="https://github.com/${GITHUB_REPOSITORY}.git"
 
-    CHART_PATH="$(echo "${cR}" | cut -d ':' -f 2)"
+    CHART_PATH="$(_jq '.chartPath')"
 
     GITHUB_REPOSITORY_TEMP="${TMP_DIR}/${GITHUB_REPOSITORY}/"
     GITHUB_REPOSITORY_TEMP_CHART="${TMP_DIR}/${GITHUB_REPOSITORY}/${CHART_PATH}"
 
     mkdir -p "${GITHUB_REPOSITORY_TEMP}"
-    git clone "${GITHUB_URL}" "${GITHUB_REPOSITORY_TEMP}" &>${OUTFILE}
+    git clone "${GITHUB_URL}" "${GITHUB_REPOSITORY_TEMP}" &>"${OUTFILE}"
     RET_CODE="${?}"
     if [[ ${RET_CODE} -ne 0 ]]; then
         cat "${OUTFILE}"
